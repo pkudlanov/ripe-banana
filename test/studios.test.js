c6576d64ce44702b7374a83c30996100e7e6f931
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-17 15:07:36 -0700

    studios routes and test set up

---
+++ test/studios.test.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+// const request = require('supertest');
+// const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+// const Studio = require('../lib/models/Studio');
+
+describe('app routes', () => {
+    beforeAll(() => {
+        connect();
+    });
+
+    beforeEach(() => {
+        return mongoose.connection.dropDatabase();
+    });
+
+    afterAll(() => {
+        return mongoose.connection.close();
+    });
+});
