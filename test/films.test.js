62abbeac0693584ec598a70ebc30ce97fc1f3770
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-18 15:41:29 -0700

    POST film TEST

--- test/films.test.js
+++ test/films.test.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 // const Film = require('../lib/models/Film');
@@ -17,5 +17,30 @@ describe('app routes', () => {
 
     afterAll(() => {
         return mongoose.connection.close();
+    });
+
+    it('creates a new film with POST', () => {
+        return request(app)
+            .post('/api/v1/films')
+            .send({
+                title: 'Two Weeks to Breaking Sound',
+                studio: '5d30f0adaa39090f44bf4db9',
+                released: 2016,
+                cast: [
+                    { role: 'Billy Gatly', actor: '7d30f0ffcd39090f26ef4db9' }
+                ]
+            })
+            .then(res => {
+                expect(res.body).toEqual({
+                    _id: expect.any(String),
+                    title: 'Two Weeks to Breaking Sound',
+                    studio: '5d30f0adaa39090f44bf4db9',
+                    released: 2016,
+                    cast: [
+                        { role: 'Billy Gatly', actor: '7d30f0ffcd39090f26ef4db9' }
+                    ],
+                    __v: 0
+                });
+            });
     });
 });
