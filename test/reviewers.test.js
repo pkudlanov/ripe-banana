69f6fbf90f6b5c56c28c0a92d5b1a07d9aad4e17
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-18 12:05:57 -0700

    POST reviewer TEST creates a new reviewer

--- test/reviewers.test.js
+++ test/reviewers.test.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 // const Reviewer = require('../lib/models/Reviewer');
@@ -18,4 +18,22 @@ describe('app routes', () => {
     afterAll(() => {
         return mongoose.connection.close();
     });
+
+    it('makes a new reviewer with POST', () => {
+        return request(app)
+            .post('/api/v1/reviewers')
+            .send({
+                name: 'Johnny Orangeseed',
+                company: 'Johnny\'s Opinions'
+            })
+            .then(res => {
+                expect(res.body).toEqual({
+                    _id: expect.any(String),
+                    name: 'Johnny Orangeseed',
+                    company: 'Johnny\'s Opinions',
+                    __v: 0
+                });
+            });
+    });
+    // reviewers will get POST, GET, GET:id, PUT
 });
