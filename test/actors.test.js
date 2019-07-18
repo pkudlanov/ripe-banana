f2cd9c0ab7a2ec240fa3ed7f864a43e805b42207
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-18 10:25:39 -0700

    POST actor test set up

--- test/actors.test.js
+++ test/actors.test.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
-// const Actor = require('../lib/models/Actor');
+const Actor = require('../lib/models/Actor');
 
 describe('app routes', () => {
     beforeAll(() => {
@@ -17,5 +17,26 @@ describe('app routes', () => {
 
     afterAll(() => {
         return mongoose.connection.close();
+    });
+
+    // post, get, get:id, put, delete(only if not in any film)
+
+    it('creates a new actor object with POST', () => {
+        return request(app)
+            .post('/api/v1/actors')
+            .send({
+                name: 'George Albertson',
+                dob: '1917-05-09',
+                pob: 'Hamburg, Germany'
+            })
+            .then(res => {
+                expect(res.body).toEqual({
+                    _id: expect.any(String),
+                    name: 'George Alberson',
+                    dob: '1917-05-09',
+                    pob: 'Hamburg, Germany',
+                    __v: 0
+                });
+            });
     });
 });
