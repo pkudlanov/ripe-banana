f043cdb9c52b78943b5b515b3030ce58ee200e4c
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-19 11:55:48 -0700

    GET reiews TEST

--- test/reviews.test.js
+++ test/reviews.test.js
@@ -77,4 +77,48 @@ describe('app routes', () => {
                 });
             });
     });
+
+    it('gets all or 100 recent reviews with GET', () => {
+        const reviews = [{
+            rating: 1,
+            reviewer: reviewer._id,
+            review: `It was a rather raunchy film to say the least.
+                Lots of slamming doors, and popping of tires. I never
+                did nor do I now, approve of such activity. That is
+                just plain and simple unacceptable in this universe
+                or any for that matter. Don't watch the film if you
+                don't want to get mentally scared for life from all
+                the door slamming and popping of tires!`,
+            film: film._id
+        }, {
+            rating: 5,
+            reviewer: reviewer._id,
+            review: 'I highly recommend. It had frogs chewing gum as the intro.',
+            film: film._id
+        }, {
+            rating: 3,
+            reviewer: reviewer._id,
+            review: `If flying erasers is your thing then the film
+                really isn't half bad. Otherwise it is just plain a
+                waste of time.`,
+            film: film._id
+        }];
+
+        return request(app)
+            .get('/api/v1/reviews')
+            .then(res => {
+                const reviewsJSON = JSON.parse(JSON.stringify(reviews));
+                reviewsJSON.forEach(review => {
+                    expect(res.body).toContainEqual({
+                        _id: review._id,
+                        rating: review.rating,
+                        review: review.review,
+                        film: {
+                            _id: film._id,
+                            film: film.title
+                        }
+                    });
+                });
+            });
+    });
 });
