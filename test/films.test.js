0bcd8781a188d84e42ea1af8d72910c7d47ddddc
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-18 17:07:40 -0700

    POST film works and passing test, test also updated

--- test/films.test.js
+++ test/films.test.js
@@ -4,11 +4,25 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Studio = require('../lib/models/Studio');
+const Actor = require('../lib/models/Actor');
 // const Film = require('../lib/models/Film');
 
 describe('app routes', () => {
     beforeAll(() => {
         connect();
+    });
+
+    let studio = null;
+    let actor = null;
+    beforeEach(async() => {
+        studio = JSON.parse(JSON.stringify(await Studio.create({ name: 'Grassy Studios' })));
+        actor = JSON.parse(JSON.stringify(await Actor.create({ name: 'Mell Grady' })));
+        // const testStudio = {
+        //     _id: '5e30fbdd34f909f844ba48b9',
+        //     name: 'Grassy Studios',
+        //     __v: 0
+        // };
     });
 
     beforeEach(() => {
@@ -24,20 +38,24 @@ describe('app routes', () => {
             .post('/api/v1/films')
             .send({
                 title: 'Two Weeks to Breaking Sound',
-                studio: '5d30f0adaa39090f44bf4db9',
+                studio: studio._id,
                 released: 2016,
                 cast: [
-                    { role: 'Billy Gatly', actor: '7d30f0ffcd39090f26ef4db9' }
+                    { role: 'Billy Gatly', actor: actor._id }
                 ]
             })
             .then(res => {
                 expect(res.body).toEqual({
                     _id: expect.any(String),
                     title: 'Two Weeks to Breaking Sound',
-                    studio: '5d30f0adaa39090f44bf4db9',
+                    studio: studio._id,
                     released: 2016,
                     cast: [
-                        { role: 'Billy Gatly', actor: '7d30f0ffcd39090f26ef4db9' }
+                        {
+                            _id: expect.any(String),
+                            role: 'Billy Gatly',
+                            actor: actor._id
+                        }
                     ],
                     __v: 0
                 });
